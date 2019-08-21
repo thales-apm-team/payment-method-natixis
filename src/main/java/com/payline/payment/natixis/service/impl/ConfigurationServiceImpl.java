8cfff62695e656bc876ea7b435cb43c3eece746c
splanard-thales <sebastien.planard@mythalesgroup.io>
2019-08-21 16:01:45 +0200

    Implémentation de ConfigurationServiceImpl#getParameters

--- src/main/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImpl.java
+++ src/main/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImpl.java
@@ -1,9 +1,13 @@
 package com.payline.payment.natixis.service.impl;
 
+import com.payline.payment.natixis.utils.Constants;
 import com.payline.payment.natixis.utils.i18n.I18nService;
 import com.payline.payment.natixis.utils.properties.ReleaseProperties;
 import com.payline.pmapi.bean.configuration.ReleaseInformation;
 import com.payline.pmapi.bean.configuration.parameter.AbstractParameter;
+import com.payline.pmapi.bean.configuration.parameter.impl.InputParameter;
+import com.payline.pmapi.bean.configuration.parameter.impl.ListBoxParameter;
+import com.payline.pmapi.bean.configuration.parameter.impl.PasswordParameter;
 import com.payline.pmapi.bean.configuration.request.ContractParametersCheckRequest;
 import com.payline.pmapi.logger.LogManager;
 import com.payline.pmapi.service.ConfigurationService;
@@ -11,14 +15,39 @@ import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
+import java.util.*;
 
 public class ConfigurationServiceImpl implements ConfigurationService {
 
     private static final Logger LOGGER = LogManager.getLogger(ConfigurationServiceImpl.class);
+
+    private static final class CategoryPurpose {
+        private static final String CASH = "CASH";
+        private static final String CORT = "CORT";
+        private static final String DVPM = "DVPM";
+        private static final String INTC = "INTC";
+        private static final String TREA = "TREA";
+    }
+
+    private static final class ChargeBearer {
+        private static final String CRED = "CRED";
+        private static final String DEBT = "DEBT";
+        private static final String SHAR = "SHAR";
+        private static final String SLEV = "SLEV";
+    }
+
+    private static final class ServiceLevel {
+        private static final String SEPA = "SEPA";
+        private static final String NURG = "NURG";
+    }
+
+    private static final class Purpose {
+        private static final String ACCT = "ACCT";
+        private static final String CASH = "CASH";
+        private static final String COMC = "COMC";
+        private static final String CPKC = "CPKC";
+        private static final String TRPT = "TRPT";
+    }
 
     private ReleaseProperties releaseProperties = ReleaseProperties.getInstance();
     private I18nService i18n = I18nService.getInstance();
@@ -27,7 +56,109 @@ public class ConfigurationServiceImpl implements ConfigurationService {
     public List<AbstractParameter> getParameters(Locale locale) {
         List<AbstractParameter> parameters = new ArrayList<>();
 
-        // TODO
+        // creditorName
+        InputParameter creditorName = new InputParameter();
+        creditorName.setKey( Constants.ContractConfigurationKeys.CREDITOR_NAME );
+        creditorName.setLabel( i18n.getMessage("contract.creditorName.label", locale) );
+        creditorName.setDescription( i18n.getMessage("contract.creditorName.description", locale) );
+        creditorName.setRequired( true );
+        parameters.add( creditorName );
+
+        // creditorIban
+        InputParameter creditorIban = new InputParameter();
+        creditorIban.setKey( Constants.ContractConfigurationKeys.CREDITOR_IBAN );
+        creditorIban.setLabel( i18n.getMessage("contract.creditorIban.label", locale) );
+        creditorIban.setDescription( i18n.getMessage("contract.creditorIban.description", locale) );
+        creditorIban.setRequired( true );
+        parameters.add( creditorIban );
+
+        // clientId
+        InputParameter clientId = new InputParameter();
+        clientId.setKey( Constants.ContractConfigurationKeys.CLIENT_ID );
+        clientId.setLabel( i18n.getMessage("contract.clientId.label", locale) );
+        clientId.setDescription( i18n.getMessage("contract.clientId.description", locale) );
+        clientId.setRequired( true );
+        parameters.add( clientId );
+
+        // clientSecret
+        PasswordParameter clientSecret = new PasswordParameter();
+        clientSecret.setKey( Constants.ContractConfigurationKeys.CLIENT_SECRET );
+        clientSecret.setLabel( i18n.getMessage("contract.clientSecret.label", locale) );
+        clientSecret.setDescription( i18n.getMessage("contract.clientSecret.description", locale) );
+        clientSecret.setRequired( true );
+        parameters.add( clientSecret );
+
+        // serviceLevel
+        ListBoxParameter serviceLevel = new ListBoxParameter();
+        serviceLevel.setKey( Constants.ContractConfigurationKeys.SERVICE_LEVEL );
+        serviceLevel.setLabel( i18n.getMessage("contract.serviceLevel.label", locale) );
+        serviceLevel.setDescription( i18n.getMessage("contract.serviceLevel.description", locale) );
+        Map<String, String> serviceLevelValues = new HashMap<>();
+        serviceLevelValues.put(ServiceLevel.SEPA, ServiceLevel.SEPA);
+        serviceLevelValues.put(ServiceLevel.NURG, ServiceLevel.NURG);
+        serviceLevel.setList( serviceLevelValues );
+        serviceLevel.setRequired( true );
+        serviceLevel.setValue( ServiceLevel.SEPA );
+        parameters.add( serviceLevel );
+
+        // localInstrument
+        ListBoxParameter localInstrument = new ListBoxParameter();
+        localInstrument.setKey( Constants.ContractConfigurationKeys.LOCAL_INSTRUMENT );
+        localInstrument.setLabel( i18n.getMessage("contract.localInstrument.label", locale) );
+        localInstrument.setDescription( i18n.getMessage("contract.localInstrument.description", locale) );
+        Map<String, String> localInstrumentValues = new HashMap<>();
+        localInstrumentValues.put("INST", "INST");
+        localInstrument.setList( localInstrumentValues );
+        localInstrument.setRequired( true );
+        localInstrument.setValue( "INST" );
+        parameters.add( localInstrument );
+
+        // categoryPurpose
+        ListBoxParameter categoryPurpose = new ListBoxParameter();
+        categoryPurpose.setKey( Constants.ContractConfigurationKeys.CATEGORY_PURPOSE );
+        categoryPurpose.setLabel( i18n.getMessage("contract.categoryPurpose.label", locale) );
+        categoryPurpose.setDescription( i18n.getMessage("contract.categoryPurpose.description", locale) );
+        Map<String, String> categoryPurposeValues = new HashMap<>();
+        categoryPurposeValues.put(CategoryPurpose.CASH, CategoryPurpose.CASH);
+        categoryPurposeValues.put(CategoryPurpose.CORT, CategoryPurpose.CORT);
+        categoryPurposeValues.put(CategoryPurpose.DVPM, CategoryPurpose.DVPM);
+        categoryPurposeValues.put(CategoryPurpose.INTC, CategoryPurpose.INTC);
+        categoryPurposeValues.put(CategoryPurpose.TREA, CategoryPurpose.TREA);
+        categoryPurpose.setList( categoryPurposeValues );
+        categoryPurpose.setRequired( true );
+        categoryPurpose.setValue( CategoryPurpose.DVPM );
+        parameters.add( categoryPurpose );
+
+        // purpose
+        ListBoxParameter purpose = new ListBoxParameter();
+        purpose.setKey( Constants.ContractConfigurationKeys.PURPOSE );
+        purpose.setLabel( i18n.getMessage("contract.purpose.label", locale) );
+        purpose.setDescription( i18n.getMessage("contract.purpose.description", locale) );
+        Map<String, String> purposeValues = new HashMap<>();
+        purposeValues.put(Purpose.ACCT, Purpose.ACCT);
+        purposeValues.put(Purpose.CASH, Purpose.CASH);
+        purposeValues.put(Purpose.COMC, Purpose.COMC);
+        purposeValues.put(Purpose.CPKC, Purpose.CPKC);
+        purposeValues.put(Purpose.TRPT, Purpose.TRPT);
+        purpose.setList( purposeValues );
+        purpose.setRequired( true );
+        purpose.setValue( Purpose.COMC );
+        parameters.add( purpose );
+
+        // chargeBearer
+        ListBoxParameter chargeBearer = new ListBoxParameter();
+        chargeBearer.setKey( Constants.ContractConfigurationKeys.CHARGE_BEARER );
+        chargeBearer.setLabel( i18n.getMessage("contract.chargeBearer.label", locale) );
+        chargeBearer.setDescription( i18n.getMessage("contract.chargeBearer.description", locale) );
+        Map<String, String> chargeBearerValues = new HashMap<>();
+        chargeBearerValues.put(ChargeBearer.CRED, ChargeBearer.CRED);
+        chargeBearerValues.put(ChargeBearer.DEBT, ChargeBearer.DEBT);
+        chargeBearerValues.put(ChargeBearer.SHAR, ChargeBearer.SHAR);
+        chargeBearerValues.put(ChargeBearer.SLEV, ChargeBearer.SLEV);
+        chargeBearer.setList( chargeBearerValues );
+        chargeBearer.setRequired( true );
+        chargeBearer.setValue( ChargeBearer.SLEV );
+        parameters.add( chargeBearer );
 
         return parameters;
     }
--- src/test/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImplTest.java
+++ src/test/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImplTest.java
@@ -3,6 +3,8 @@ package com.payline.payment.natixis.service.impl;
 import com.payline.payment.natixis.utils.i18n.I18nService;
 import com.payline.payment.natixis.utils.properties.ReleaseProperties;
 import com.payline.pmapi.bean.configuration.ReleaseInformation;
+import com.payline.pmapi.bean.configuration.parameter.AbstractParameter;
+import com.payline.pmapi.bean.configuration.parameter.impl.ListBoxParameter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -11,11 +13,9 @@ import org.mockito.MockitoAnnotations;
 
 import java.text.SimpleDateFormat;
 import java.time.Month;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
@@ -31,6 +31,45 @@ class ConfigurationServiceImplTest {
     @BeforeEach
     void setup(){
         MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void getName(){
+        // given: a name is configured
+        String testName = "Natixis payment method";
+        doReturn( testName ).when( i18n ).getMessage(anyString(), any(Locale.class));
+
+        // when: calling the method getName
+        String name = service.getName( Locale.getDefault() );
+
+        // then: the method returns the name
+        assertEquals( testName, name );
+    }
+
+    @Test
+    void getParameters() {
+        // given: every call to internationalization service returns a string
+        doReturn( "value" ).when( i18n ).getMessage( anyString(), any(Locale.class) );
+
+        // when: retrieving the contract parameters
+        List<AbstractParameter> parameters = service.getParameters( Locale.getDefault() );
+
+        // then: each parameter has a unique key, a label and a description. List box parameters have at least 1 possible value.
+        List<String> keys = new ArrayList<>();
+        for( AbstractParameter param : parameters ){
+            // 2 different parameters should not have the same key
+            assertFalse( keys.contains( param.getKey() ) );
+            keys.add( param.getKey() );
+
+            // each parameter should have a label and a description
+            assertNotNull( param.getLabel() );
+            assertNotNull( param.getDescription() );
+
+            // in case of a ListBoxParameter, it should have at least 1 value
+            if( param instanceof ListBoxParameter ){
+                assertFalse( ((ListBoxParameter) param).getList().isEmpty() );
+            }
+        }
     }
 
     @Test
@@ -52,19 +91,6 @@ class ConfigurationServiceImplTest {
         assertEquals(2019, releaseInformation.getDate().getYear());
         assertEquals(Month.AUGUST, releaseInformation.getDate().getMonth());
         assertEquals(19, releaseInformation.getDate().getDayOfMonth());
-    }
-
-    @Test
-    void getName(){
-        // given: a name is configured
-        String testName = "Natixis payment method";
-        doReturn( testName ).when( i18n ).getMessage(anyString(), any(Locale.class));
-
-        // when: calling the method getName
-        String name = service.getName( Locale.getDefault() );
-
-        // then: the method returns the name
-        assertEquals( testName, name );
     }
 
 }
