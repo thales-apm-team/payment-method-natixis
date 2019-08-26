13539a87fb76172fa589b2712b21bc42ad0869bc
splanard-thales <sebastien.planard@mythalesgroup.io>
2019-08-26 15:48:31 +0200

    Suppression d'un logger non utilisé (Sonar)

--- src/main/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImpl.java
+++ src/main/java/com/payline/payment/natixis/service/impl/ConfigurationServiceImpl.java
@@ -13,17 +13,13 @@ import com.payline.pmapi.bean.configuration.parameter.impl.ListBoxParameter;
 import com.payline.pmapi.bean.configuration.parameter.impl.PasswordParameter;
 import com.payline.pmapi.bean.configuration.request.ContractParametersCheckRequest;
 import com.payline.pmapi.bean.payment.ContractProperty;
-import com.payline.pmapi.logger.LogManager;
 import com.payline.pmapi.service.ConfigurationService;
-import org.apache.logging.log4j.Logger;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class ConfigurationServiceImpl implements ConfigurationService {
-
-    private static final Logger LOGGER = LogManager.getLogger(ConfigurationServiceImpl.class);
 
     private static final class CategoryPurpose {
         private static final String CASH = "CASH";
